c44744da1c565985f63667354dc1b45ae5835952
Malak El Oualy <malak.eloualy218@outlook.fr>
2024-01-04 15:56:54 +0100

    First Commit + Added the validators in the Product Model and removed the method Check..

---
+++ P3AddNewFunctionalityDotNetCore/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P3AddNewFunctionalityDotNetCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ILanguageService _languageService;
+
+        public ProductController(IProductService productService, ILanguageService languageService)
+        {
+            _productService = productService;
+            _languageService = languageService;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ProductViewModel> products = _productService.GetAllProductsViewModel();
+            return View(products);
+        }
+
+        [Authorize]
+        public IActionResult Admin()
+        {
+            return View(_productService.GetAllProductsViewModel().OrderByDescending(p => p.Id));
+        }
+
+        [Authorize]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(ProductViewModel product)
+        {
+              
+
+           
+
+            if (ModelState.IsValid)//Si toutes les données saisies sont validées
+            {
+                _productService.SaveProduct(product);//Enregistrer le produit
+                return RedirectToAction("Admin");//Retounrer a la page d'accueil
+            }
+            else
+            {
+                return View(product);//Sinon afficher la vue Product
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.DeleteProduct(id);
+            return RedirectToAction("Admin");
+        }
+    }
+}
---
+++ P3AddNewFunctionalityDotNetCore/Models/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace P3AddNewFunctionalityDotNetCore.Models.Entities
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderLine = new HashSet<OrderLine>();
+        }
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "MissingName")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "MissingName")]
+        public string Details { get; set; }
+        [Required(ErrorMessage = "MissingName")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "MissingPrice")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "PriceNotANumber")]
+        [Range(1, int.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "MissingQuantity")]
+    
+        [RegularExpression("^[0-9]+$", ErrorMessage = "StockNotANumber")]
+        [Range(1, int.MaxValue, ErrorMessage = "StockNotGreaterThanZero")]
+        public int Quantity { get; set; }
+
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
